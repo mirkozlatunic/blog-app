917a11128eb57faa65a14593949f134cec91f1f8
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-08-29 21:08:29 -0700

    display validation

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -22,18 +22,22 @@ export default function TopicCreateForm() {
       <PopoverContent>
         <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
-            <h3 className="text-lg">Create a Topc</h3>
+            <h3 className="text-lg">Create a Topic</h3>
             <Input
               name="name"
               label="Name"
               labelPlacement="outside"
               placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(", ")}
             />
             <Textarea
               name="description"
-              label="description"
+              label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(", ")}
             />
             <Button type="submit">Submit</Button>
           </div>
